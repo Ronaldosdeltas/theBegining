1a4a805745093897dc2a455b72ab387e9df5087c
ronaldoss8 <ronaldosssantos1@gmail.coml>
2024-12-07 16:17:26 -0300

    Initial commit

---
+++ src/main/java/iniciadoNoSpring/theBegining/TheBeginingApplication.java
@@ -0,0 +1,13 @@
+package iniciadoNoSpring.theBegining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBeginingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBeginingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iniciadoNoSpring/theBegining/TheBeginingApplicationTests.java
@@ -0,0 +1,13 @@
+package iniciadoNoSpring.theBegining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBeginingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
